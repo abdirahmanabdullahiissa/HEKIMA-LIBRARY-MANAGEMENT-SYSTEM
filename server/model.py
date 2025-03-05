5e2f3f30519c8febb0b13d3dcbaeee3a2cfe6902
abdirahmanabdullahiissa <abdirahman.abdullahi@student.moringaschool.com>
2025-03-05 18:06:39 +0300

    created new  tables

--- server/model.py
+++ server/model.py
@@ -46,3 +46,29 @@ class UserRole(db.Model,SerializerMixin):
     user_id=db.Column(db.Integer,db.ForeignKey("users.id"),primary_key=True)
     role_id=db.Column(db.Integer, db.ForeignKey("roles.id"),primary_key=True)
 
+class Book(db.Model,SerializerMixin):
+    __tablename__="books"
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String,nullable=False)
+    author=db.Column(db.String,nullable=False)
+    isbn=db.Column(db.String(13),unique=True, nullable=False)
+    publication_year=db.Column(db.Integer, nullable=True)
+    copies_available=db.Column(db.Integer,default=1, nullable=False)
+
+    def serialize(self):
+        return {"id":self.id,"name":self.name,"author":self.author,"publication_year":self.publication_year,"copies_available":self.copies_available}
+
+
+class BorrowedBook(db.Model):
+    __tablename__ = "borrowed_books"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+    borrowed_at = db.Column(db.DateTime, default=datetime.utcnow)
+    due_date = db.Column(db.DateTime, nullable=False)
+    returned_at = db.Column(db.DateTime, nullable=True)
+    user = db.relationship('User', backref='borrowed_books')
+    book = db.relationship('Book', backref='borrowed_books')
+
+    def serialize(self):
+        return {"id":self.id,"borrowed_at":self.borrowed_at,"due_date":self.due_date,"returned_at":self.returned_at}
